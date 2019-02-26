2587a8d28cbd1d2d59cd35c78fbd7e554e1ac93f
Jakub Siębor <j.siebor@qnt.pl>
2019-02-26 09:27:44 +0100

    siema

---
+++ projekcik/src/projekcik.java
@@ -0,0 +1,13 @@
+package j.siebor.github.integration;
+
+import javax.swing.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(
+                null,
+                "Welcome on GitHub",
+                "Information",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}
